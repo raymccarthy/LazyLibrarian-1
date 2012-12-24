606cc0f4eda358f38b7d0596eced15ac2a77884b
LibrarianMike <mrg14r@gmail.com>
2012-12-24 21:50:04 +1100

    Postprocessing fix

--- lazylibrarian/postprocess.py
+++ lazylibrarian/postprocess.py
@@ -7,6 +7,8 @@ import lazylibrarian
 from lazylibrarian import database, logger, formatter
 
 def processDir():
+    logger.info('Postprocessing has begun.')
+	
     # rename this thread
     threading.currentThread().name = "POSTPROCESS"
 
@@ -26,97 +28,102 @@ def processDir():
             if book['NZBtitle'] in downloads:
                 logger.debug('nzbtitle ' + book['NZBtitle'] + ' is in downloads')
                 pp_path = os.path.join(processpath, book['NZBtitle'])
-                logger.debug('Found folder %s.' % pp_path)
 
-                data = myDB.select("SELECT * from books WHERE BookID='%s'" % book['BookID'])
-                for metadata in data:
-                    authorname = metadata['AuthorName']
-                    authorimg = metadata['AuthorLink']
-                    bookname = metadata['BookName']
-                    bookdesc = metadata['BookDesc']
-                    bookisbn = metadata['BookIsbn']
-                    bookrate = metadata['BookRate']
-                    bookimg = metadata['BookImg']
-                    bookpage = metadata['BookPages']
-                    booklink = metadata['BookLink']
-                    bookdate = metadata['BookDate']
-                    booklang = metadata['BookLang']
-                    bookpub = metadata['BookPub']
+                if (os.path.exists(pp_path)):
+                	logger.debug('Found folder %s.' % pp_path)
 
-                try:
-                    os.chmod(os.path.join(lazylibrarian.DESTINATION_DIR, authorname).encode(lazylibrarian.SYS_ENCODING), 0777);
-                except Exception, e:
-                    logger.debug("Could not chmod author directory");
+                	data = myDB.select("SELECT * from books WHERE BookID='%s'" % book['BookID'])
+                	for metadata in data:
+                		authorname = metadata['AuthorName']
+                		authorimg = metadata['AuthorLink']
+                		bookname = metadata['BookName']
+                		bookdesc = metadata['BookDesc']
+                		bookisbn = metadata['BookIsbn']
+                		bookrate = metadata['BookRate']
+                		bookimg = metadata['BookImg']
+                		bookpage = metadata['BookPages']
+                		booklink = metadata['BookLink']
+                		bookdate = metadata['BookDate']
+                		booklang = metadata['BookLang']
+                		bookpub = metadata['BookPub']
 
-                dest_path = authorname + os.sep + bookname
-                dic = {'<':'', '>':'', '=':'', '?':'', '"':'', ',':'', '*':'', ':':'', ';':'', '\'':''}
-                dest_path = formatter.latinToAscii(formatter.replace_all(dest_path, dic))
-                dest_path = os.path.join(lazylibrarian.DESTINATION_DIR, dest_path).encode(lazylibrarian.SYS_ENCODING)
+                	try:
+                		os.chmod(os.path.join(lazylibrarian.DESTINATION_DIR, authorname).encode(lazylibrarian.SYS_ENCODING), 0777);
+                	except Exception, e:
+                		logger.debug("Could not chmod author directory");
 
-                processBook = processDestination(pp_path, dest_path, authorname, bookname)
+                	dest_path = authorname + os.sep + bookname
+                	dic = {'<':'', '>':'', '=':'', '?':'', '"':'', ',':'', '*':'', ':':'', ';':'', '\'':''}
+                	dest_path = formatter.latinToAscii(formatter.replace_all(dest_path, dic))
+                	dest_path = os.path.join(lazylibrarian.DESTINATION_DIR, dest_path).encode(lazylibrarian.SYS_ENCODING)
 
-                if processBook:
+                	processBook = processDestination(pp_path, dest_path, authorname, bookname)
 
-                    ppcount = ppcount+1
+                	if processBook:
 
-                    # try image
-                    processIMG(dest_path, bookimg)
+                		ppcount = ppcount+1
 
-                    # try metadata
-                    processOPF(dest_path, authorname, bookname, bookisbn, book['BookID'], bookpub, bookdate, bookdesc, booklang)
+                		# try image
+                		processIMG(dest_path, bookimg)
 
-                    #update nzbs
-                    controlValueDict = {"NZBurl": book['NZBurl']}
-                    newValueDict = {"Status": "Success"}
-                    myDB.upsert("wanted", newValueDict, controlValueDict)
+                		# try metadata
+                		processOPF(dest_path, authorname, bookname, bookisbn, book['BookID'], bookpub, bookdate, bookdesc, booklang)
 
-                    #update books
-                    controlValueDict = {"BookID": book['BookID']}
-                    newValueDict = {"Status": "Open"}
-                    myDB.upsert("books", newValueDict, controlValueDict)
+                    	#update nzbs
+                		controlValueDict = {"NZBurl": book['NZBurl']}
+                		newValueDict = {"Status": "Success"}
+                		myDB.upsert("wanted", newValueDict, controlValueDict)
 
-                    #update authors
-                    query = 'SELECT COUNT(*) FROM books WHERE AuthorName="%s" AND (Status="Have" OR Status="Open")' % authorname
-                    countbooks = myDB.action(query).fetchone()
-                    havebooks = int(countbooks[0])
-                    controlValueDict = {"AuthorName": authorname}
-                    newValueDict = {"HaveBooks": havebooks}
-                    myDB.upsert("authors", newValueDict, controlValueDict)
+                    	#update books
+                		controlValueDict = {"BookID": book['BookID']}
+                		newValueDict = {"Status": "Open"}
+                		myDB.upsert("books", newValueDict, controlValueDict)
 
-                    logger.info('Successfully processed: %s - %s' % (authorname, bookname))
-                else:
-                    logger.info('Postprocessing for %s has failed.' % bookname)
+                    	#update authors
+                		query = 'SELECT COUNT(*) FROM books WHERE AuthorName="%s" AND (Status="Have" OR Status="Open")' % authorname
+                		countbooks = myDB.action(query).fetchone()
+                		havebooks = int(countbooks[0])
+                		controlValueDict = {"AuthorName": authorname}
+                		newValueDict = {"HaveBooks": havebooks}
+                		myDB.upsert("authors", newValueDict, controlValueDict)
+
+                		logger.info('Successfully processed: %s - %s' % (authorname, bookname))
+                	else:
+                		logger.info('Postprocessing for %s has failed.' % bookname)
         if ppcount:
             logger.info('%s books are downloaded and processed.' % ppcount)
+        else:
+            logger.info('No snatched books have been found')
 
 def processDestination(pp_path=None, dest_path=None, authorname=None, bookname=None):
 
     if not os.path.exists(dest_path):
         logger.debug('%s does not exist, so it\'s safe to create it' % dest_path)
-        try:
-            if lazylibrarian.DESTINATION_COPY:
-                shutil.copytree(pp_path, dest_path)
-                logger.info('Successfully copied %s to %s.' % (pp_path, dest_path))
-            else:
-                shutil.move(pp_path, dest_path)
-                logger.info('Successfully moved %s to %s.' % (pp_path, dest_path))
-            pp = True
-            #try and rename the actual book file
-            for file2 in os.listdir(dest_path):
-            	logger.debug('file extension: ' + str(file2).split('.')[-1])
-            	if ((file2.lower().find(".jpg") <= 0) & (file2.lower().find(".opf") <= 0)):
-            		logger.debug('file: ' + str(file2))
-            		os.rename(os.path.join(dest_path, file2), os.path.join(dest_path, bookname + '.' + str(file2).split('.')[-1]))
-            try:
-                os.chmod(dest_path, 0777);
-            except Exception, e:
-                logger.info("Could not chmod path: " + str(dest_path));
-        except OSError:
-            logger.error('Could not create destination folder or rename the downloaded ebook. Check permissions of: ' + lazylibrarian.DESTINATION_DIR)
-            pp = False
     else:
-        pp = False
-    return pp
+        logger.debug('%s already exsists. It will be overwritten' % dest_path)
+	try:
+		if lazylibrarian.DESTINATION_COPY:
+			shutil.copytree(pp_path, dest_path)
+			logger.debug('Successfully copied %s to %s.' % (pp_path, dest_path))
+		else:
+			shutil.move(pp_path, dest_path)
+			logger.debug('Successfully moved %s to %s.' % (pp_path, dest_path))
+		pp = True
+        
+		#try and rename the actual book file
+		for file2 in os.listdir(dest_path):
+			logger.debug('file extension: ' + str(file2).split('.')[-1])
+			if ((file2.lower().find(".jpg") <= 0) & (file2.lower().find(".opf") <= 0)):
+				logger.debug('file: ' + str(file2))
+				os.rename(os.path.join(dest_path, file2), os.path.join(dest_path, bookname + '.' + str(file2).split('.')[-1]))
+		try:
+			os.chmod(dest_path, 0777);
+		except Exception, e:
+			logger.debug("Could not chmod path: " + str(dest_path));
+	except OSError:
+		logger.info('Could not create destination folder or rename the downloaded ebook. Check permissions of: ' + lazylibrarian.DESTINATION_DIR)
+		pp = False
+	return pp
 
 def processIMG(dest_path=None, bookimg=None):
     #handle pictures
